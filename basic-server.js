ddaf33ce7015ce447b27f5f19ecddc3d8b930ecb
[Franko Leka] <[franko9leka@gmail.com]>
2023-04-11 22:52:21 -0400

    messed around with chalk

--- basic-server.js
+++ basic-server.js
@@ -1,1 +1,30 @@
 console.log ("We will make this work")
+
+const chalk = require('chalk');
+const log = console.log;
+
+// Basic Colors
+
+log(chalk.red('Firered'));
+log(chalk.white('boooo!'));
+log(chalk.yellow('happy halloween!'));
+
+// Background colors
+
+log(chalk.bgRed('yum'));
+log(chalk.bgGreen('link'));
+
+// Styling & Decoration
+
+log(chalk.bold('bold and brash'));
+log(chalk.underline('clearly important'));
+
+// Chaining
+
+log(chalk.yellow.bgGreen.bold('Banana & Monky'));
+log(chalk.white.bgRed.underline('Homeland'));
+
+// Hex & RGB
+
+log(chalk.hex('#009779').bold('special'));
+log(chalk.rgb(19, 199, 55).bold('close to birth'));
